a765236f9351f1104f99fbe9055618d323a720d3
Malder82PChome <pto.nz@yandex.ru>
2023-11-26 16:41:29 +0600

    Begin study C#

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// the code asking username and displaying Hello "UserName"
+Console.Write("What your name: ");
+// WritLine - выводит запрос на следующей линии, после описания запроса
+// если использовать просто Write - запрос и описание запроса будут в одну линию
+string username = Console.ReadLine();
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
