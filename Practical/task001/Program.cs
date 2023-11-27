16c54500720d7dc6691ffdc656dcfe3cf41d7314
Malder82PChome <pto.nz@yandex.ru>
2023-11-28 00:21:08 +0600

    UPD, do practical task#1,2

---
+++ Practical/task001/Program.cs
@@ -0,0 +1,31 @@
+﻿// this code checks if the first number is the square of the second
+while(true)
+{
+    Console.Clear();
+    Console.Write("Enter first number - ");
+    string strnumA = Console.ReadLine();
+    int.TryParse(strnumA, out int numA); // convert from type "string" to type "int"
+    Console.Write("Enter second number - ");
+    string strnumB = Console.ReadLine();
+    int.TryParse(strnumB, out int numB);
+    int numcheck = numB * numB;
+    if(numcheck == numA)
+    {
+        Console.WriteLine("yes, first number is the square of the second");
+    }
+    else
+    {
+        Console.WriteLine("No, first number isn't the square of the second");
+    }
+    Console.Write("Do You wnant to continue? (Y/N): ");
+    string usercheck = Console.ReadLine();
+    if(usercheck.ToLower() == "n" || usercheck.ToLower() == "exit" || usercheck.ToLower() == "н" || usercheck.ToLower() == "q") //one of the conditions is met
+    {
+        Console.WriteLine("Exiting the program. Goodbye!");
+        break; //exit from while 
+    }
+    else
+    {
+        Console.Clear();
+    }
+}
---
+++ Practical/task002/Program.cs
@@ -0,0 +1,22 @@
+﻿// program that takes as input integer N, and the output shows all integers numbers in the range from -N to N.
+// Examples 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+
+Console.Clear();
+
+Console.Write("Enter a positive integer - ");
+string strFirstNum = Console.ReadLine();
+int.TryParse(strFirstNum, out int FirstNum); // convert from type "string" to type "int"
+int reversedNum = -FirstNum;
+
+Console.WriteLine($"range of numbers from {FirstNum} to {reversedNum}");
+
+int arraySize = FirstNum - reversedNum + 1; // set the size for the array
+int[] resultArray = new int[arraySize]; // create the array itself (empty) with size "arraySize"
+
+for(int i = 0; i != arraySize; i++)
+{
+    resultArray[i] = reversedNum + i;
+}
+Console.WriteLine(string.Join(" ", resultArray));
+// 'Console.WriteLine($"Your array: {resultArray}");' you can not display array use this code !!!!!
+// Displaing array use 'string.Join'
