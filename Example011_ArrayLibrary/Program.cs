b89e2c4222a0743239e42730cf287e7caf670cde
Malder82PChome <pto.nz@yandex.ru>
2023-12-03 21:14:22 +0600

    create new exp

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿// create new array and full and print this array use method
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+        {
+            Console.WriteLine(col[position]);
+            position++;
+        }
+}
+
+int[] array = new int[10]; //create new array with 10 elements
+
+FillArray(array);
+PrintArray(array);
