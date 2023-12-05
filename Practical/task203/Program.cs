e4f9871f9f374ea2256effdf3e1e8d3ce888370b
Malder82PChome <pto.nz@yandex.ru>
2023-12-05 23:25:50 +0600

    do task204, need delete comments

---
+++ Practical/task203/Program.cs
@@ -0,0 +1,25 @@
+﻿//program that will take two inputs
+//numbers and print whether the second number is a multiple of the first.
+//If the second number is not a multiple of the first, then the program displays
+//remainder of the division
+//14,5 -> no, 4; 16, 8 -> yes
+
+Console.Clear();
+
+Console.Write("Enter first number - ");
+string strFirstNum = Console.ReadLine();
+int.TryParse(strFirstNum, out int FirstNum);
+Console.Write("Enter second number - ");
+string strSecondNum = Console.ReadLine();
+int.TryParse(strSecondNum, out int SecondNum);
+
+int Result = FirstNum % SecondNum;
+if (Result == 0)
+{
+    Console.WriteLine("Yes, the second number is multiple of the first number");
+}
+
+else
+{
+    Console.WriteLine($" No, the second number is not multiple of the first number. Remainder of the division - {Result}");
+}
---
+++ Practical/task204/Program.cs
@@ -0,0 +1,50 @@
+﻿//program that prints third from end
+//digit of a given number or reports that there is no third digit
+//456 => 6
+//7812 => 8
+//91 => There is no third digit
+
+Console.Clear();
+
+Console.Write("Enter number - ");
+string strNum = Console.ReadLine();
+int.TryParse(strNum, out int Num);
+
+//if (Num == 0)
+//{
+    //Console.WriteLine($"The number of digits in a number `{Num}` is equal to - ONE");//There is no third digit
+    //return;
+//}
+
+//else
+//{
+    int absNum = Math.Abs(Num); // Get the absolute value of a number to handle negative numbers
+
+int count = 0;
+
+while (absNum > 0)
+{
+    absNum = absNum / 10; // can use "absNum /= 10"
+    count++;
+}
+
+//Console.WriteLine($"Number of digits in a number {Num}: {count}");
+
+int[] Array = new int[count]; //create new array with size == Number on our digital
+
+for (int i = count - 1; i >= 0; i--)
+    {
+        Array[i] = Num % 10; // get last digital in number
+        Num /= 10; // delet last digital in number (Num = Num / 10)
+    }
+if (count < 3)
+{
+    Console.WriteLine("There is no third digit");
+}
+else
+{
+    int Result = Array[count - 3];
+    Console.WriteLine($"The third digit from the end of the number `{strNum}` is `{Result}`");
+}
+//}
+
